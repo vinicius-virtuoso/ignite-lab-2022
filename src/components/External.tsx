5e2b76d6d01ff8fd4a158bcb63a27f41059d7103
Vinicius Virtuoso <viniciusvirtuoso1902@gmail.com>
2022-06-28 17:21:27 -0300

    ajuste reponsividade

--- src/components/External.tsx
+++ src/components/External.tsx
@@ -12,16 +12,16 @@ export function External({ href, icon, title, children }: External) {
     <a
       href={href}
       target="_blank"
-      className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-0 hover:bg-gray-600 transition-colors flex-col lg:flex-row"
+      className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-1 hover:bg-gray-600 transition-colors lg:flex-row lg:gap-6"
     >
       <div className="bg-blue-500 h-full p-6 flex items-center justify-center lg:justify-start">
         {icon}
       </div>
-      <div className="p-6 lg:px-0 leading-relaxed text-center">
-        <strong className="text-2xl ">{title}</strong>
+      <div className="p-2 lg:px-0 leading-relaxed lg:text-left lg:py-8">
+        <strong className="text-1xl lg:text-2xl">{title}</strong>
         <p className="text-sm text-gray-200 mt-2">{children}</p>
       </div>
-      <div className="h-full p-6 flex items-center justify-center text-blue-300">
+      <div className="h-full p-1 flex items-center justify-center text-blue-300 lg:p-6">
         <CaretRight size={24} />
       </div>
     </a>
--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -4,15 +4,15 @@ export function Subscribe() {
   return (
     <>
       <div className="min-h-screen w-full bg-blur bg-no-repeat bg-cover flex flex-col items-center">
-        <div className="max-w-[1100px] flex items-center justify-center text-center mx-auto mt-10 w-full flex-col">
-          <div className="max-w-[640px] flex flex-col justify-center items-center px-4">
+        <div className="max-w-[1100px] flex lg:flex-row items-center justify-center text-center mx-auto mt-10 lg:mt-20 w-full flex-col lg:justify-between">
+          <div className="max-w-[640px] flex flex-col justify-center lg:items-start items-center px-4 lg:text-left">
             <Logo w="200" h="37" />
             <h1 className="mt-6 lg:text-[2.5rem] leading-tight text-2xl">
               Construa uma
               <strong className="text-blue-200"> aplicação completa</strong>, do
               zero, com <strong className="text-blue-200">React</strong>.
             </h1>
-            <p className="text-gray-200 my-4 leading-relaxed text-sm opacity-80">
+            <p className="text-gray-200 my-4 leading-relaxed text-sm opacity-80 lg:text-lg">
               Em apenas uma semana você vai dominar na prática uma das
               tecnologias mais utilizadas e com alta demanda para acessar as
               melhores oportunidades do mercado.
@@ -47,11 +47,11 @@ export function Subscribe() {
         </div>
         <img
           src="/src/assets/code-mockup.png"
-          className="mt-10 lg:max-w-[1100px] w-full max-w-full"
+          className="mt-10 lg:max-w-[1100px] w-full max-w-[90%]"
           alt="code-mockup"
         />
         <footer className="flex items-center gap-4 border-t border-gray-500 justify-center w-full bg-gray-700">
-          <div className=" flex items-center gap-4 py-5 max-w-[1100px] w-full">
+          <div className=" flex items-center justify-center lg:justify-start gap-4 py-5 max-w-[1100px] w-full">
             <span className="text-sm">Desenvolvido por Vinícius Virtuoso</span>
             <Logo w="120" h="37" />
           </div>
