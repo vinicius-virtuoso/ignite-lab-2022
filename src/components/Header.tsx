bcc2fa0e951b9c37e27a26b98cab3c8f583c4d18
Vinicius Virtuoso <viniciusvirtuoso1902@gmail.com>
2022-06-28 16:27:19 -0300

    ajuste de tela

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,7 +7,7 @@ export function Header() {
   const { isOpen, setIsOpen } = useContext(MenuContext);
 
   return (
-    <header className="relative w-full py-5 px-5 flex items-center justify-between bg-gray-700 border-b border-gray-600 xl:justify-center">
+    <header className="relative w-full py-5 px-5 flex items-center justify-between bg-gray-700 border-b border-gray-600 xl:justify-center h-[75px]">
       <Logo w="170" h="37" />
 
       <div className="xl:hidden" onClick={() => setIsOpen(!isOpen)}>
